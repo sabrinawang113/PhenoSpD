58eab9a90c5524be972a72a92bdda0e0ee887030
sabrinawang113 <151745704+sabrinawang113@users.noreply.github.com>
2024-03-21 17:22:15 +0100

    Add files via upload

---
+++ ferkingstad_2021_PMID34857953/join-files-rs92.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=join-files-rs92
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=20-1:0:00
+#SBATCH --mem=100000M
+#SBATCH --partition=high_p
+
+cd /data/GWAS_data/work/000_GWAS_formatting/ferkingstad_2021_PMID34857953/005_phenospd/002_extract-info_proteins_cis-SNPs
+
+Rscript join-files-rs92.R
